2876169783acbde3fdf16c095616ed678f87863c
Tapish-git <83629917+Tapish-git@users.noreply.github.com>
2024-10-17 23:07:36 +0530

    possible solution

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 #The problem was to create a graph using the input list and check which algorithm will be best
+
+
+
+'''
+Input: root = [1,null,3,2,4,null,5,6]
+'''
+
+'''
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+'''
--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,24 @@
-'''
-Input: root = [1,null,3,2,4,null,5,6]
-'''
+from typing import List
 
-'''
-Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
-'''
+class Node:
+    def __init__(self, val=None, children=[]):
+        self.val = val
+        self.children = children
 
+class Solution:
+    
+    def createTree(self, input: List) -> Node:
+        if not input:
+            return None
+        
+        root = Node(input[0])
+        queue = [root]
+        
+        while queue:
+            current =  queue.pop(0)
+            while input[0] is None:
+                child = Node(input[1])
+                current.children.append(child)
+                queue.append(child)
 
+        return root

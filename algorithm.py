cd9e6ce9bf5b82108a99a7a0eb4244af573e059f
Tapish-git <83629917+Tapish-git@users.noreply.github.com>
2024-10-17 23:21:03 +0530

    Added comments

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,6 @@
 from typing import List
 
+#Defined a node
 class Node:
     def __init__(self, val=None, children=[]):
         self.val = val
@@ -11,17 +12,18 @@ class Solution:
         if not input:
             return None
         
-        root = Node(input[0])
-        queue = [root]
-        ptr = 1
+        root = Node(input[0])           #Create a root node for tree   
+        queue = [root]          #Putt the root node into queue, for keeping track of each level
+        ptr = 1             #ptr to traverse the input list
         
         while queue and ptr < len(input):
-            current = queue.pop(0)
-            while input[ptr] is not None:
-                child = Node(input[ptr])
-                current.children.append(child)
-                queue.append(child)
+            current = queue.pop(0)          #Make this as a parent node
+            while input[ptr] is not None and ptr < len(input):
+                child = Node(input[ptr])        #create a new node with value at current index in input 
+                current.children.append(child)          #add a new child node to the children of current node
+                queue.append(child)         #add child to queue, tp process further
                 ptr += 1
-            ptr += 1
+            ptr += 1                #once processed all children of the current node, increment to skip the null
 
         return root
+    
